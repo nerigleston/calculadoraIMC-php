1bf3a86fe38c74559865ca3e8b98c8e306d35294
Neri Filho <nerifilho1256@gmail.com>
2023-09-25 15:21:59 -0300

    altereações estilizações

--- form.php
+++
@@ -1,6 +0,0 @@
-<h1>Calculadora de IMC</h1>
-    <form method="post" action="imc.php">
-        Peso (em quilogramas): <input type="text" name="peso"><br>
-        Altura (em metros): <input type="text" name="altura"><br>
-        <input type="submit" value="Calcular IMC">
-    </form>
--- imc.php
+++ imc.php
@@ -1,24 +1,57 @@
-<?php
-        $peso = $_POST["peso"];
-        $altura = $_POST["altura"];
-        {
-          $imc = $peso / ($altura * $altura);
-            if ($imc < 17) {
-                $tipoDePeso = "Muito abaixo do peso";
-            } elseif ($imc >= 17 && $imc < 18.49) {
-                $tipoDePeso = "Abaixo do peso";
-            } elseif ($imc >= 18.5 && $imc < 24.99) {
-                $tipoDePeso = "Peso normal";
-            } elseif ($imc >= 25 && $imc < 29.99) {
-                $tipoDePeso = "Acima do peso";
-            } elseif ($imc >= 30 && $imc < 34.99) {
-                $tipoDePeso = "Obesidade tipo 1";
-            } elseif ($imc >= 35 && $imc < 39.99) {
-                $tipoDePeso = "Obesidade tipo 2 (severa)";
-            } else {
-                $tipoDePeso = "Obesidade tipo 3 (mórbida)";
-            }
-            echo "<h2>Seu IMC é: " . number_format($imc, 2) . "</h2>";
-            echo "<p>Seu peso é: $tipoDePeso</p>";
-        }
-    ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <style>
+    h2 {
+      color: #fff;
+      text-align: center;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      padding: 40px;
+      border-radius: 5px;
+    }
+
+    body {
+      background-size: cover;
+      background-attachment: fixed;
+      background-image: url(./gettyimages-641963334-612x612.jpg);
+    }
+
+    h2,
+    p {
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+  </style>
+</head>
+
+<body>
+  <?php
+  $peso = $_POST["peso"];
+  $altura = $_POST["altura"];
+
+  $imc = $peso / ($altura * $altura);
+
+  if ($imc < 17) {
+    $tipoDePeso = "Muito abaixo do peso";
+  } elseif ($imc >= 17 && $imc < 18.49) {
+    $tipoDePeso = "Abaixo do peso";
+  } elseif ($imc >= 18.5 && $imc < 24.99) {
+    $tipoDePeso = "Peso normal";
+  } elseif ($imc >= 25 && $imc < 29.99) {
+    $tipoDePeso = "Acima do peso";
+  } elseif ($imc >= 30 && $imc < 34.99) {
+    $tipoDePeso = "Obesidade tipo 1";
+  } elseif ($imc >= 35 && $imc < 39.99) {
+    $tipoDePeso = "Obesidade tipo 2 (severa)";
+  } else {
+    $tipoDePeso = "Obesidade tipo 3 (mórbida)";
+  }
+
+  echo "<h2>Seu IMC é: " . number_format($imc, 2) . "</p> Seu peso é: $tipoDePeso </h2>";
+  ?>
+</body>
+
+</html>
